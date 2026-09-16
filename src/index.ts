88b10f15730ad00fdc8e91b6c4f3f0056836e777
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-09-16 09:33:21 +0200

    feat: added the rateLimiter

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import {sessionRoutes} from "./presentation/http/routes/session.routes.js";
 import {userRoutes} from "./presentation/http/routes/user.routes.js";
 import {audioRoutes} from "./presentation/http/routes/audio.routes.js";
 import {MinioStorageService} from "./infrastructure/storage/MinioStorageService.js";
+import fastifyRateLimit from "@fastify/rate-limit";
 
 const fastify = Fastify({ logger: true }).withTypeProvider<TypeBoxTypeProvider>()
 await registerJwt(fastify);
@@ -21,7 +22,15 @@ await fastify.register(multipart, {
     limits: {
         fileSize: 30 * 1024 * 1024,
     },
-});await fastify.register(authRoutes, { prefix: "/auth" });
+});
+
+await fastify.register(fastifyRateLimit, {
+    max: 30,
+    timeWindow: '1 minute',
+    keyGenerator: (request) => request.ip
+})
+
+await fastify.register(authRoutes, { prefix: "/auth" });
 await fastify.register(sessionRoutes, { prefix: "/session" });
 await fastify.register(userRoutes, { prefix: "/user" });
 await fastify.register(audioRoutes, { prefix: "/audio" });
