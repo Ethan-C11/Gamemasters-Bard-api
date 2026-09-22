e1908daa770fc51e46770d1d3b043562c5de2740
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-09-22 17:58:32 +0200

    feat: added the cleaning of user sounds when the user is deleted

--- src/application/useCases/user/DeleteUserUseCase.ts
+++ src/application/useCases/user/DeleteUserUseCase.ts
@@ -1,17 +1,18 @@
-import { Session } from "../../../infrastructure/db/entities/session.entity.js";
 import {User} from "../../../infrastructure/db/entities/user.entity.js";
 import {AppDataSource} from "../../../infrastructure/db/AppDataSource.js";
 import {Repository} from "typeorm";
-import {generateRandomString} from "../../../shared/utils/GenerateRandomString.js";
-import {PasswordHasher} from "../../../shared/utils/PasswordHasher.js";
+import {AudioTrack} from "../../../infrastructure/db/entities/audioTrack.entity.js";
+import {MinioStorageService} from "../../../infrastructure/storage/MinioStorageService.js";
 
 export class DeleteUserUseCase {
 
     private static _instance: DeleteUserUseCase;
 
+    private _audioTrackRepository: Repository<AudioTrack>;
     private _userRepository: Repository<User>;
 
     private constructor() {
+        this._audioTrackRepository = AppDataSource.getRepository(AudioTrack);
         this._userRepository = AppDataSource.getRepository(User);
     }
 
@@ -27,7 +28,29 @@ export class DeleteUserUseCase {
         if (!user)
             throw Error("User not found");
 
+        try{
+            const userSounds = await this._audioTrackRepository.findBy({ uploadedBy: {id: user.id}, isUserImported: true });
+            let folders : string[] = []
 
-        return await this._userRepository.remove(user);
+            for (const soundTrack of userSounds) {
+                await MinioStorageService.getInstance().delete(soundTrack.storageKey);
+                await this._audioTrackRepository.remove(soundTrack)
+                const folderArray = soundTrack.storageKey.split("/")
+                const folder = folderArray[0] + "/" + folderArray[1] + "/" + folderArray[2];
+                folders.push(folder)
+            }
+
+            folders = [...new Set(folders)]
+            for (const folder of folders)
+            {
+                await MinioStorageService.getInstance().delete(folder);
+            }
+
+            const removedUser = await this._userRepository.remove(user);
+            removedUser.id = userId;
+            return removedUser;
+        } catch(err) {
+            throw err;
+        }
     }
 }
